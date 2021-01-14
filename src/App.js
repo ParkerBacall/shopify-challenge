a359f4cc78640d6f12dca4403d92a227c8fd05e5
parkerbacall <Pbacall2@gmail.com>
2021-01-13 21:32:27 -0800

    router, styling, pop-up-content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Home from './routes/Home'
 import About from './routes/About'
